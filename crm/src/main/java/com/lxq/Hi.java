19fe6c1d445bd64691259f9b85fea545b07ebc29
lxq <97081527+AshitaInf@users.noreply.github.com>
2024-04-21 19:06:11 +0800

    第一版 H1

---
+++ crm/src/main/java/com/lxq/Hi.java
@@ -0,0 +1,13 @@
+package com.lxq;
+
+/**
+ * @author :星期天
+ * @version :1.0
+ * @date :2024/4/21 19:02
+ */
+
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("H1");
+    }
+}
